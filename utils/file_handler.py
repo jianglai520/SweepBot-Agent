8764434886bebbf1afa0b1cd1d807c961233112f
Jianglai <lingxg2024@lzu.edu.cn>
2026-08-06 19:07:40 +0800

    feat: 修复知识库加载路径于文本编码问题

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -42,7 +42,7 @@ def listdir_with_allowed_type(path: str, allowed_types: tuple[str]):
 
     if not os.path.isdir(path):
         logger.error(f"[listdir_with_allowed_type]{path}不是文件夹")
-        return allowed_types
+        return ()
 
     for f in os.listdir(path):
         if f.endswith(allowed_types):
@@ -55,4 +55,4 @@ def pdf_loader(filepath: str, passwd = None)-> list[Document]:
 
 
 def txt_loader(filepath: str)-> list[Document]:
-    return TextLoader(filepath).load()
+    return TextLoader(filepath, encoding = "utf-8").load()
